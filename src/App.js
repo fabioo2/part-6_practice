75cce640cb7c4984aa723a33b500683c40979121
Fabio Kim <fabio.kim2@gmail.com>
2021-11-09 13:58:18 -0500

    started practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from 'redux';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const counterReducer = (state = 0, action) => {
+        switch (action.type) {
+            case 'INCREMENT':
+                return state + 1;
+            case 'DECREMENT':
+                return state - 1;
+            case 'ZERO':
+                return 0;
+            default:
+                return state;
+        }
+    };
+
+    const store = createStore(counterReducer);
+    store.subscribe(() => {
+        const storeNow = store.getState();
+        console.log('the store is now', storeNow);
+    });
+    store.dispatch({ type: 'INCREMENT' });
+    store.dispatch({ type: 'INCREMENT' });
+    store.dispatch({ type: 'INCREMENT' });
+    store.dispatch({ type: 'ZERO' });
+    store.dispatch({ type: 'DECREMENT' });
+
+    return (
+        <div className="App">
+            <h1>sup</h1>
+        </div>
+    );
 }
 
 export default App;
