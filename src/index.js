4907e0cb03b89db8c8084d18bc959dc07928afa9
Fabio Kim <fabio.kim@d2l.com>
2021-11-09 15:22:01 -0500

    finished practice for 6.1-6.2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { createStore } from 'redux';
+import noteReducer from './reducers/noteReducer';
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const store = createStore(noteReducer);
+
+store.dispatch({
+    type: 'NEW_NOTE',
+    data: {
+        content: 'the app state is in redux store',
+        important: true,
+        id: 1,
+    },
+});
+
+store.dispatch({
+    type: 'NEW_NOTE',
+    data: {
+        content: 'state changes are made with actions',
+        important: false,
+        id: 2,
+    },
+});
+
+const App = () => {
+    return (
+        <div>
+            <ul>
+                {store.getState().map((note) => (
+                    <li key={note.id}>
+                        {note.content} <strong>{note.important ? 'important' : ''}</strong>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+const renderApp = () => {
+    ReactDOM.render(<App />, document.getElementById('root'));
+};
+
+renderApp();
+store.subscribe(renderApp);
