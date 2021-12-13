701e83ccccc4146ef6e79467734e76f8a9e2c2c7
Fabio Kim <fabio.kim2@gmail.com>
2021-12-12 20:39:28 -0500

    finished hook api version

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import App from './App';
 
+// import noteReducer, { initializeNotes } from './reducers/noteReducer';
 import noteReducer from './reducers/noteReducer';
 import filterReducer from './reducers/filterReducer';
 
-import { createNote } from './reducers/noteReducer';
-import { filterChange } from './reducers/filterReducer';
+// import noteService from './services/notes';
 
 const reducer = combineReducers({
     notes: noteReducer,
@@ -18,6 +18,16 @@ const reducer = combineReducers({
 });
 
 const store = createStore(reducer, composeWithDevTools());
+
+// * we don't use this because its not as efficient as creating a new action that intializes all the notes at once instead of iterating through the array and dispatching the create note action for each note.
+// noteService.getAll().then((notes) =>
+//     notes.forEach((note) => {
+//         store.dispatch({ type: 'NEW_NOTE', data: note });
+//     })
+// );
+
+// * this is more efficient but it's more ideal to initialize notes in the App component
+//noteService.getAll().then((notes) => store.dispatch(initializeNotes(notes)));
 
 // store.dispatch(filterChange('IMPORTANT'));
 // store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'));
